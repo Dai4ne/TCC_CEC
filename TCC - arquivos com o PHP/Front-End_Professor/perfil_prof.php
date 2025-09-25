d7a859a5c62e9d97c94dd50a51b04eacd502a3a0
Leandro Jardim <leandrosilva6167@gmail.com>
2025-09-25 12:45:04 -0300

    ARRUMANDO A PÁGINA DE HOME DO PROFESSOR

--- TCC - arquivos com o PHP/Front-End_Professor/perfil_prof.php
+++ TCC - arquivos com o PHP/Front-End_Professor/perfil_prof.php
@@ -2,13 +2,13 @@
 <html lang="pt-BR">
 
 <head>
-  <meta charset="UTF-8" />
-  <meta name="viewport" content="width=device-width, initial-scale=1.0" />
-  <title>CEC</title>
-  <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/css/bootstrap.min.css" rel="stylesheet">
-  <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.13.1/font/bootstrap-icons.min.css">
+    <meta charset="UTF-8" />
+    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+    <title>CEC</title>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/css/bootstrap.min.css" rel="stylesheet">
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.13.1/font/bootstrap-icons.min.css">
 
-  <style>
+    <style>
         body {
             margin: 0;
             padding: 0;
