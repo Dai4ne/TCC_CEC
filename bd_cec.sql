2994dd25588c5a27215ebd1818234cf8c0f9ee05
Arthur Gbr <artthur408@gmail.com>
2025-11-07 12:12:44 -0300

    Add files via upload

--- bd_cec.sql
+++ bd_cec.sql
@@ -29,7 +29,6 @@ SET time_zone = "+00:00";
 --
 -- Estrutura da tabela `emprestimo`
 --
-
 CREATE TABLE `emprestimo` (
   `id_emprestimo` int(11) NOT NULL,
   `data_hora` datetime NOT NULL DEFAULT current_timestamp(),
@@ -52,12 +51,10 @@ CREATE TABLE `equipamento` (
   `descricao` varchar(500) NOT NULL,
   `numero_serie` varchar(20) NOT NULL,
   `id_marca` int(11) NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 -- --------------------------------------------------------
 
---
--- Estrutura da tabela `local`
 --
 
 CREATE TABLE `local` (
@@ -65,9 +62,6 @@ CREATE TABLE `local` (
   `nome` varchar(80) DEFAULT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
--- --------------------------------------------------------
-
---
 -- Estrutura da tabela `marca`
 --
 
@@ -75,6 +69,194 @@ CREATE TABLE `marca` (
   `id_marca` int(11) NOT NULL,
   `nome` varchar(50) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+-- Inserindo dados na tabela `marca`
+
+
+INSERT INTO `marca` (`nome`) VALUES 
+('Samsung'),
+('Google'),
+('Positivo'),
+('Lenovo'),
+('LG'),
+('Outro');
+
+-- --------------------------------------------------------
+
+--
+-- Estrutura da tabela `notificacao`
+--
+
+CREATE TABLE `notificacao` (
+  `id_notificacao` int(11) NOT NULL,
+  `id_remetente` int(11) NOT NULL,
+  `id_destinatario` int(11) NOT NULL,
+  `mensagem` varchar(400) NOT NULL,
+  `data_envio` datetime NOT NULL DEFAULT current_timestamp(),
+  `status_notificacao` char(1) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+-- --------------------------------------------------------
+
+--
+-- Estrutura da tabela `ocorrencias`
+--
+
+CREATE TABLE `ocorrencias` (
+  `id_ocorrencia` int(11) NOT NULL,
+  `relato` varchar(300) NOT NULL,
+  `data_relato` datetime NOT NULL DEFAULT current_timestamp(),
+  `status` char(1) NOT NULL,
+  `id_equipamento` int(11) NOT NULL,
+  `id_usuario` int(11) NOT NULL,
+  `id_emprestimo` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+-- --------------------------------------------------------
+
+--
+-- Estrutura da tabela `usuario`
+--
+
+CREATE TABLE `usuario` (
+  `id_usuario` int(11) NOT NULL,
+  `nome` varchar(100) NOT NULL,
+  `email` varchar(120) NOT NULL,
+  `senha` varchar(50) NOT NULL,
+  `tipo` char(1) NOT NULL,
+  `data_registro` datetime NOT NULL DEFAULT current_timestamp()
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Índices para tabelas despejadas
+--
+
+--
+-- Índices para tabela `emprestimo`
+--
+ALTER TABLE `emprestimo`
+  ADD PRIMARY KEY (`id_emprestimo`),
+  ADD KEY `id_usuario` (`id_usuario`),
+  ADD KEY `id_equipamento` (`id_equipamento`);
+
+--
+-- Índices para tabela `equipamento`
+--
+ALTER TABLE `equipamento`
+  ADD PRIMARY KEY (`id_equipamento`),
+  ADD UNIQUE KEY `numero_serie` (`numero_serie`),
+  ADD KEY `fk_equipamento_marca` (`id_marca`);
+
+--
+-- Índices para tabela `local`
+--
+ALTER TABLE `local`
+  ADD PRIMARY KEY (`id_local`);
+
+--
+-- Índices para tabela `marca`
+--
+ALTER TABLE `marca`
+  ADD PRIMARY KEY (`id_marca`);
+
+--
+-- Índices para tabela `notificacao`
+--
+ALTER TABLE `notificacao`
+  ADD PRIMARY KEY (`id_notificacao`),
+  ADD KEY `id_remetente` (`id_remetente`),
+  ADD KEY `id_destinatario` (`id_destinatario`);
+
+--
+-- Índices para tabela `ocorrencias`
+--
+ALTER TABLE `ocorrencias`
+  ADD PRIMARY KEY (`id_ocorrencia`),
+  ADD KEY `id_equipamento` (`id_equipamento`),
+  ADD KEY `id_usuario` (`id_usuario`),
+  ADD KEY `id_emprestimo` (`id_emprestimo`);
+
+--
+-- Índices para tabela `usuario`
+--
+ALTER TABLE `usuario`
+  ADD PRIMARY KEY (`id_usuario`),
+  ADD UNIQUE KEY `email` (`email`);
+
+--
+-- AUTO_INCREMENT de tabelas despejadas
+--
+
+--
+-- AUTO_INCREMENT de tabela `emprestimo`
+--
+ALTER TABLE `emprestimo`
+  MODIFY `id_emprestimo` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT de tabela `equipamento`
+--
+ALTER TABLE `equipamento`
+  MODIFY `id_equipamento` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT de tabela `marca`
+--
+ALTER TABLE `marca`
+  MODIFY `id_marca` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT de tabela `notificacao`
+--
+ALTER TABLE `notificacao`
+  MODIFY `id_notificacao` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT de tabela `ocorrencias`
+--
+ALTER TABLE `ocorrencias`
+  MODIFY `id_ocorrencia` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT de tabela `usuario`
+--
+ALTER TABLE `usuario`
+  MODIFY `id_usuario` int(11) NOT NULL AUTO_INCREMENT;
+
+
+
+--
+-- Limitadores para a tabela `emprestimo`
+--
+ALTER TABLE `emprestimo`
+  ADD CONSTRAINT `emprestimo_ibfk_1` FOREIGN KEY (`id_usuario`) REFERENCES `usuario` (`id_usuario`),
+  ADD CONSTRAINT `emprestimo_ibfk_2` FOREIGN KEY (`id_equipamento`) REFERENCES `equipamento` (`id_equipamento`);
+
+--
+-- Limitadores para a tabela `equipamento`
+--
+ALTER TABLE `equipamento`
+  ADD CONSTRAINT `fk_equipamento_marca` FOREIGN KEY (`id_marca`) REFERENCES `marca` (`id_marca`);
+
+--
+-- Limitadores para a tabela `notificacao`
+--
+ALTER TABLE `notificacao`
+  ADD CONSTRAINT `notificacao_ibfk_1` FOREIGN KEY (`id_remetente`) REFERENCES `usuario` (`id_usuario`),
+  ADD CONSTRAINT `notificacao_ibfk_2` FOREIGN KEY (`id_destinatario`) REFERENCES `usuario` (`id_usuario`);
+
+--
+-- Limitadores para a tabela `ocorrencias`
+--
+ALTER TABLE `ocorrencias`
+  ADD CONSTRAINT `ocorrencias_ibfk_1` FOREIGN KEY (`id_equipamento`) REFERENCES `equipamento` (`id_equipamento`),
+  ADD CONSTRAINT `ocorrencias_ibfk_2` FOREIGN KEY (`id_usuario`) REFERENCES `usuario` (`id_usuario`),
+  ADD CONSTRAINT `ocorrencias_ibfk_3` FOREIGN KEY (`id_emprestimo`) REFERENCES `emprestimo` (`id_emprestimo`);
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
 
 -- --------------------------------------------------------
 
